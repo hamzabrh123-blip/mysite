894e087764ebdfcaf7bc4f602c18b1039fdad62d
Hamza <hamzabrh123@gmail.com>
2025-12-02 22:08:13 +0530

    Admin OTP + new admin system added

--- mynews/models.py
+++ mynews/models.py
@@ -48,3 +48,5 @@ class AdminOTP(models.Model):
     email = models.EmailField()
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.email} - {self.otp}"
